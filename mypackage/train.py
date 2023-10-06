fada7c63bc69a7413a7bdaddd7b3e2b26ba851c8
Sergey <agibalov1294@gmail.com>
2023-10-07 01:23:53 +0300

    Added pre-commit

--- mypackage/train.py
+++ mypackage/train.py
@@ -2,37 +2,39 @@ import joblib
 import numpy as np
 import pandas as pd
 import requests
-from catboost import CatBoostClassifier
-from catboost import Pool
+from catboost import CatBoostClassifier, Pool
 from imblearn.over_sampling import SMOTE
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
 
 try:
-    f = open('dataset.csv')
+    f = open("dataset.csv")
     f.close()
 except FileNotFoundError:
-    url = 'https://drive.google.com/uc?export=download&id=1HJTDfP6njwgsToIrSSPBNNRXvh9QmX_R'
+    file_id = "1HJTDfP6njwgsToIrSSPBNNRXvh9QmX_R"
+    url = f"https://drive.google.com/uc?export=download&id={file_id}"
     r = requests.get(url)
     while r.status_code != 200:
         r = requests.get(url)
-    with open('dataset.csv', 'wb') as f:
+    with open("dataset.csv", "wb") as f:
         f.write(r.content)
 
 print("File downloaded")
 
 df = pd.read_csv("dataset.csv")
-target = 'Dx:Cancer'
+target = "Dx:Cancer"
 df = df.drop_duplicates()
 
 for name in df.columns:
-    mean = np.array(df[f'{name}'] != "?").mean()
+    mean = np.array(df[f"{name}"] != "?").mean()
     df[name] = df[name].replace({"?": mean})
 
 X = df.drop(columns=target)
 y = df[target]
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42, stratify=y)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.33, random_state=42, stratify=y
+)
 sm = SMOTE()
 x_train_sm, y_train_sm = sm.fit_resample(X_train, y_train)
 
@@ -45,14 +47,9 @@ X_train_scal = scaler.transform(X_train)
 
 
 def fit_model(train_pool):
-    model = CatBoostClassifier(
-        iterations=1000
-    )
+    model = CatBoostClassifier(iterations=1000)
 
-    return model.fit(
-        train_pool,
-        silent=True
-    )
+    return model.fit(train_pool, silent=True)
 
 
 train_pool = Pool(x_train_sm, y_train_sm)
